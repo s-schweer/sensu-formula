542d3d532963d2cd44e2952d513e8a77444c461e
Stefan Schweer <s.schweer@douglas.de>
2018-02-15 23:39:07 +0100

    add centos support for tests - add git installation by yum - add iproute installation to make grains work as expected

--- tests/integration/default/testinfra/test_sensu_server.py
+++ tests/integration/default/testinfra/test_sensu_server.py
@@ -1,20 +1,19 @@
-# def test_file_exists(host):
-#     foo = host.file('/foo.yml')
-#     assert foo.exists
-#     assert foo.contains('your')
+def test_config_directory_exists(host):
+    conf_d = host.file('/etc/sensu/conf.d')
+    assert conf_d.exists
+    assert conf_d.is_directory
 
 def test_sensu_is_installed(host):
     sensu = host.package('sensu')
     assert sensu.is_installed
 
 
-# def test_user_and_group_exist(host):
-#     user = host.user('foo')
-#     assert user.group == 'foo'
-#     assert user.home == '/var/lib/foo'
-#
-#
-# def test_service_is_running_and_enabled(host):
-#     foo = host.service('foo')
-#     assert foo.is_enabled
-#     assert foo.is_running
+def test_user_and_group_exist(host):
+    user = host.user('sensu')
+    assert user.group == 'sensu'
+    assert user.home == '/opt/sensu'
+
+def test_service_is_running_and_enabled(host):
+    sensu_server = host.service('sensu-server')
+    assert sensu_server.is_enabled
+    assert sensu_server.is_running
